32e474ab9445217298a6d00a725d94e24f13fbbe
Ranadip Das <115198050+Randip07@users.noreply.github.com>
2026-02-05 13:59:20 +0530

    VERSION 1.4

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const predictRoutes = require("./routes/predict.routes");
 const app = express();
 app.use(cors());
 
-app.use("/api", predictRoutes);
 
-const PORT = 5000;
+
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
+
+app.use("/api", predictRoutes);
